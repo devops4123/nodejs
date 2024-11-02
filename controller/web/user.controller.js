47d2da8e8e49c94f660b4872e9dccba076903369
Vikash Tiwari <Teamcoordinator@earninghandle.com>
2024-11-02 13:57:27 +0530

    login signup admin or user completed

---
+++ controller/web/user.controller.js
@@ -0,0 +1,64 @@
+const User = require('../../Models/web/user.model.js');
+const { hashPassword, comparePassword } = require('../../utils/bcrypt.js');
+const { generateToken } = require('../../utils/jwt.js');
+
+// User Signup
+exports.signup = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await hashPassword(password);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// User Login
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = generateToken(user._id);
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get All Users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get User by ID
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Update User
+exports.updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
